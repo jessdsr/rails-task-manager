024f6d53427ba41d8e0af33a554483b441ed7d88
Jessica Ribeiro <jessica.ribeiro@hotmail.fr>
2022-05-17 13:05:16 +0200

    task manager including CRUD actions and refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # The `new` route needs to be placed before the `show` route
+
+  # # CREATE
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+
+  # # READ
+  # get 'tasks', to: 'tasks#index'
+
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # UPDATE: get the form and edit the form
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # DELETE
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  # REFACTORING
+  resources :tasks
 end
